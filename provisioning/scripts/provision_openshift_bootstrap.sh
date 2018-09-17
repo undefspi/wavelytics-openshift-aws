835c151e2371fe9ce71b1ac3d700fa5db4d9a682
undefspi <chris.beech@wavelytics.co.uk>
2018-09-17 22:40:30 +0100

    create symlinks successfully

--- provisioning/scripts/provision_openshift_bootstrap.sh
+++ provisioning/scripts/provision_openshift_bootstrap.sh
@@ -4,13 +4,19 @@
 # Ansible to deploy from the playbooks in this repositorie.  Using the cloudformation, 
 # the repo is cloned and the relevant ansible playbooks can be run.
 
+# The bootstrap natively deploys an openshift cluster which contains the following
+# Openshift-Ansible Repo  + Openshift-ansible-wavelytics under the configuration folder
+# If you use a different repo and use the params below this may break the userData 
+# script if you do not follow the same dir approach.  It is fairly simple though
+
 ## CloudFormation Parameters
 # KeyName: The standard aws keypair you would use to provision and connect to boxes
-# RepoURL: Ok so you dont want to use this repo to use - maybe fork off then ;) - Add this yourself below
+# RepoURL: Ok so you dont want to use this repo to use - maybe fork off then ;)
 # InvContext: Inventory Dir to reference your static + dynamic hosts file and vars.  This uses a custom playbook
 # to role out the setup of nodes before the openshift submodule provided ones. however, if you want to ditch that
 # this bootstrap can still be used with a differnet playbook and invnetory.
 # in the parameters section
+# WorkingOSAnsibleBranch:  Change the working branch of the openshift-ansible submodule
 
 [[ -z $1 ]] && { echo "No supplied aws keypair"; exit 1; }
 
