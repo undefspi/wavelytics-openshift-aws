08a11473b819d65a5c5a54210b5f176123aacb00
undefspi <chris.beech@wavelytics.co.uk>
2018-09-17 07:36:07 +0100

    Make Symlinks for dynamic host running for any repo

--- provisioning/scripts/provision_openshift_bootstrap.sh
+++ provisioning/scripts/provision_openshift_bootstrap.sh
@@ -4,9 +4,12 @@
 # Ansible to deploy from the playbooks in this repositorie.  Using the cloudformation, 
 # the repo is cloned and the relevant ansible playbooks can be run.
 
-## PARAMETERS
-# AWS_KEYPAIR_NAME - The standard aws keypair you would use to provision and connect to boxes
-# RepoURL - Ok so you dont want to use this repo to use - maybe fork off then ;) - Add this yourself below 
+## CloudFormation Parameters
+# KeyName: The standard aws keypair you would use to provision and connect to boxes
+# RepoURL: Ok so you dont want to use this repo to use - maybe fork off then ;) - Add this yourself below
+# InvContext: Inventory Dir to reference your static + dynamic hosts file and vars.  This uses a custom playbook
+# to role out the setup of nodes before the openshift submodule provided ones. however, if you want to ditch that
+# this bootstrap can still be used with a differnet playbook and invnetory.
 # in the parameters section
 
 [[ -z $1 ]] && { echo "No supplied aws keypair"; exit 1; }
